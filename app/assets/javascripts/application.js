a4216e90351f245fc98845e669896eb160aefc35
yoshimurayuuto <0615yutoyoshimu@gmail.com>
2020-02-19 12:04:57 +0900

    css修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,25 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery/dist/jquery.js
 //= require_tree .
+
+
+
+
+$(document).ready(function () {
+  $("#topBtn").hide(); //とりあえず隠す
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) { //100以上にスクロールされた／されている時は
+      $("#topBtn").fadeIn("fast"); //ふわっと表示
+    } else { //それ意外は
+      $("#topBtn").fadeOut("fast"); //ふわっと非表示
+    }
+  });
+  $('#topBtn').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+});
